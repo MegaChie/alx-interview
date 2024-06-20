c9e73dcaf752079f2c5aa04bd8046879f72f28f5
MegaChie <workspace.mega@gmail.com>
2024-06-20 16:23:22 -0700

    Fix/Kill bug

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -13,8 +13,8 @@ try:
     for line in sys.stdin:
         try:
             intel = line.split(" ")
+            size += int(intel[-1])
             if len(intel[-2]) == 3:
-                size += int(intel[-1])
                 codes[int(intel[-2])] = codes.get(int(intel[-2]), 0) + 1
                 limit += 1
         except (IndexError, ValueError):
