6045825d44d098e575fd42b413b050d4d5c4f81e
MegaChie <workspace.mega@gmail.com>
2024-07-31 17:31:57 -0700

    Island perimeter calculator

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,31 @@
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid.
+
+    Args:
+    - grid: A representaion of the island map where water is 0 and land is 1.
+    """
+    if grid:
+        perimeter = 0
+        length = len(grid)
+        width = len(grid[0])
+
+        for i in range(length):
+            for j in range(width):
+                if grid[i][j] == 1:
+                    # Checks up
+                    if i == 0 or grid[i - 1][j] == 0:
+                        perimeter += 1
+                    # Checks left
+                    if j == 0 or grid[i][j - 1] == 0:
+                        perimeter += 1
+                    # Checks down
+                    if i == length - 1 or grid[i + 1][j] == 0:
+                        perimeter += 1
+                    # Checks right
+                    if j == width - 1 or grid[i][j + 1] == 0:
+                        perimeter += 1
+
+        return perimeter
+
+    return 0
